cb417ff06071020b6f440226cae89cbd3f0f9518
Jihad <anbousjihad@gmail.com>
2025-08-24 22:59:17 +1000

    Created component models

--- components/admin.py
+++ components/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
 
+from .models import (
+    ComponentType,
+    Component,
+    ComponentPoint,
+)
+
 # Register your models here.
+
+
+@admin.register(ComponentType)
+class ComponentTypeAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+    )
+
+
+class ComponentPointInline(admin.TabularInline):
+    model = ComponentPoint
+    extra = 0
+
+
+@admin.register(ComponentPoint)
+class ComponentPointAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+        "x",
+        "y",
+    )
+
+
+@admin.register(Component)
+class ComponentAdmin(admin.ModelAdmin):
+    inlines = [
+        ComponentPointInline,
+    ]
+    list_display = (
+        "id",
+        "type",
+        "description",
+    )
--- components/models.py
+++ components/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+
+
+class ComponentType(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Component(models.Model):
+    type = models.ForeignKey(
+        ComponentType, on_delete=models.CASCADE, related_name="type_componenttype"
+    )
+    description = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return f"{self.type} - {self.description}"
+
+
+class ComponentPoint(models.Model):
+    component = models.ForeignKey(
+        Component, on_delete=models.CASCADE, related_name="component_component"
+    )
+    name = models.CharField(max_length=100, unique=True)
+    x = models.FloatField(help_text="[mm]")
+    y = models.FloatField(help_text="[mm]")
+
+    def __str__(self):
+        return f"{self.name}: {self.x}, {self.y}"
